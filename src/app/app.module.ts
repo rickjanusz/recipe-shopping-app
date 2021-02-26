72c1c0ecd4a6ed00bc9bba0be8cbb3a8fb568e79
Rick Janusz <rickjanusz@gmail.com>
2021-02-26 15:26:12 -0600

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
-
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { ShoppingListComponent } from './shopping-list/shopping-list.component'
+import { RecipesComponent } from './recipes/recipes.component'
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component'
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component'
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component'
+import { ListEditComponent } from './shopping-list/list-edit/list-edit.component'
+import { HeaderComponent } from './header/header.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ListEditComponent,
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
