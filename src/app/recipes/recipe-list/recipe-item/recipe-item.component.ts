6274a6cbd983bb8fac5f32e571d38dfd6024c2d3
Rick Janusz <rickjanusz@gmail.com>
2021-02-26 17:42:02 -0600

    recipes, shopping list components

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { Recipe } from '../recipe.model'
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,8 +7,6 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
-  recipes = []
-
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { Recipe } from './recipe.model'
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,18 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'A TEST recipe',
+      'https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555449015/shape/mentalfloss/istock_000035287506_small.jpg?itok=MczNwF73'
+    ),
+    new Recipe(
+      'Test Recipe',
+      'A TEST recipe',
+      'https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555449015/shape/mentalfloss/istock_000035287506_small.jpg?itok=MczNwF73'
+    ),
+  ]
   constructor() {}
 
   ngOnInit(): void {}
