660d2d45b408fb3cddf417088d70d9c3f6409157
Rick Janusz <rickjanusz@gmail.com>
2021-03-02 07:39:32 -0600

    add ingredients

--- src/app/shopping-list/list-edit/list-edit.component.ts
+++ src/app/shopping-list/list-edit/list-edit.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  Output,
+  ViewChild,
+  EventEmitter,
+} from '@angular/core'
+import { Ingredient } from 'src/app/shared/ingredient.model'
 
 @Component({
   selector: 'app-list-edit',
   templateUrl: './list-edit.component.html',
-  styleUrls: ['./list-edit.component.css']
+  styleUrls: ['./list-edit.component.css'],
 })
 export class ListEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef
+  @ViewChild('amountInput') amountInputRef: ElementRef
 
-  constructor() { }
+  @Output() ingredientAdded = new EventEmitter<Ingredient>()
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value
+    const ingAmt = this.amountInputRef.nativeElement.value
+    const newIngredient = new Ingredient(ingName, ingAmt)
+    this.ingredientAdded.emit(newIngredient)
   }
-
 }
