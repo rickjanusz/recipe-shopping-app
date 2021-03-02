cd983668cae70b3bb634b91ad04f290558b8473f
Rick Janusz <rickjanusz@gmail.com>
2021-03-01 18:08:50 -0600

    temp data binding for recipe detail

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -7,7 +7,15 @@ import { Recipe } from '../recipe.model'
   styleUrls: ['./recipe-item.component.css'],
 })
 export class RecipeItemComponent implements OnInit {
+  @Input() recipe: Recipe
+  @Output() recipeSelected = new EventEmitter<void>()
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelected() {
+    this.recipeSelected.emit()
+    console.log('Recipe-item: ', this.recipe)
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 import { Recipe } from './recipe.model'
 
 @Component({
@@ -7,19 +7,25 @@ import { Recipe } from './recipe.model'
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
+
   recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe',
-      'A TEST recipe',
+      'Test Recipe 1',
+      'A TEST recipe 1',
       'https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555449015/shape/mentalfloss/istock_000035287506_small.jpg?itok=MczNwF73'
     ),
     new Recipe(
-      'Test Recipe',
-      'A TEST recipe',
+      'Test Recipe 2',
+      'A TEST recipe 2',
       'https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555449015/shape/mentalfloss/istock_000035287506_small.jpg?itok=MczNwF73'
     ),
   ]
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe)
+  }
 }
